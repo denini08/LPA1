42f46762c8c29ed68e3c943cc186d5b205c26a69
denini08 <denini08@hotmail.com>
2016-06-01 15:58:08 -0300

    Add files via upload

---
+++ LPA1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct dados{
+	int quantidade;
+	char* nome;
+	float preço;
+	
+};
+void inciar_dados(){ 			/* vai ser a 1° função para criar o aquivo e adicionar as coisas a ele*/
+	FILE *arquivo;
+	output= fopen(nome_arq,"r");
+	
+}
+
+int main(int argc,char agr*v[]){
+
+
+
+}
